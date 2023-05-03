d419924ff348a45c3a74dfa9ced6a96877ad937a
DkAngelo <angelochiacchio03@gmail.com>
2023-05-03 18:18:11 +0200

    Updated Vector Read (ElemType directory)

--- Elemtype/Vector read/vettore.c
+++ Elemtype/Vector read/vettore.c
@@ -4,32 +4,32 @@
 
 Vector* VectorRead(const char* filename) {
 
-	if (filename == NULL) {
+	if (filename == NULL) {								//controllo se la stringa è NULL
 		return NULL;
 	}
 	FILE* f = fopen(filename, "r");
-	if (f == NULL) {
+	if (f == NULL) {									//controllo che l'apertura sia andata a buon fine
 		return NULL;
 	}
 	
-	Vector* Ret = malloc(sizeof(Vector));
+	Vector* Ret = malloc(sizeof(Vector));				//creo la struct Vector da tornare
 	Ret->size = 0;
 	Ret->capacity = 1;
 	Ret->data = malloc(sizeof(ElemType));
 
 	while (1) {
 		ElemType tmp;
-		int a = ElemRead(f, &tmp);
-		if (a <= 0) {
+		int a = ElemRead(f, &tmp);						//provo a leggere un elemento di tipo ElemType
+		if (a <= 0) {									//se la lettura non va a buon fine, interrompo il ciclo
 			break;
 		}
 		else {
-			if (Ret->capacity == Ret->size) {
+			if (Ret->capacity == Ret->size) {			//se il vettore non può contenere altri elementi, raddoppio la capacità
 				Ret->capacity *= 2;
 				Ret->data = realloc(Ret->data, Ret->capacity * sizeof(ElemType));
 			}
-			Ret->data[Ret->size] = tmp;
-			(Ret->size)++;
+			Ret->data[Ret->size] = tmp;					//inserisco l'elemento letto in coda
+			(Ret->size)++;								//aumento size (num.elementi nel vettore)
 		}
 	}
 
@@ -39,41 +39,42 @@ Vector* VectorRead(const char* filename) {
 
 Vector* VectorReadSorted(const char* filename) {
 
-	if (filename == NULL) {
+	if (filename == NULL) {						//controllo se la stringa è NULL
 		return NULL;
 	}
 	FILE* f = fopen(filename, "r");
-	if (f == NULL) {
+	if (f == NULL) {							//controllo che l'apertura sia andata a buon fine
 		return NULL;
 	}
 
-	Vector* Ret = malloc(sizeof(Vector));
+	Vector* Ret = malloc(sizeof(Vector));		//crea la struct Vector da ritornare
 	Ret->size = 0;
 	Ret->capacity = 1;
 	Ret->data = malloc(sizeof(ElemType));
 
 	while (1) {
 		ElemType tmp;
-		int a = ElemRead(f, &tmp);
-		if (a <= 0) {
+		int a = ElemRead(f, &tmp);				//provo a leggere un elemento di tipo ElemType
+		if (a <= 0) {							//se la lettura non va a buon fine, interrompo il ciclo
 			break;
 		}
 		else {
-			if (Ret->capacity == Ret->size) {
+			if (Ret->capacity == Ret->size) {		//se il vettore non può contenere un altro elemento, raddoppio la capacità
 				Ret->capacity *= 2;
 				Ret->data = realloc(Ret->data, Ret->capacity * sizeof(ElemType));
 			}
 
-			size_t minpos = Ret->size;
+			size_t minpos = Ret->size;																	//minpos sarà la posizione nel quale mettere l'elemento letto. 
+																										//Se è maggiore rispetto al resto del vettore, verrà messo in coda
 			for (size_t i = 0; i < Ret->size; i++) {
-				if (ElemCompare(&tmp, &Ret->data[i]) < 0) {
-					minpos = i;
-					memcpy(Ret->data + i + 1, Ret->data + i, sizeof(ElemType) * (Ret->size - i));
+				if (ElemCompare(&tmp, &Ret->data[i]) < 0) {												//l'elemento letto è minore di un elemento del vettore
+					minpos = i;																			//salvo la posizione in minpos
+					memmove(Ret->data + i + 1, Ret->data + i, sizeof(ElemType) * (Ret->size - i));		//sposto gli elementi (da minpos verso destra) a destra di una posizione
 					break;
 				}
 			}
-			Ret->data[minpos] = tmp;
-			(Ret->size)++;
+			Ret->data[minpos] = tmp;							//metto l'elemento letto nella posizione salvata
+			(Ret->size)++;										//aumento la size (num.elementi del vettore)
 		}
 	}
 	fclose(f);
